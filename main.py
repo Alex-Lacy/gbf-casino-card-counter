3dc8747ed2997a719108a44275cb23c38a23d3c7
Alex Lacy <alex4keys@gmail.com>
2021-03-30 15:49:57 -0600

    Added replayability to one execution

--- main.py
+++ main.py
@@ -27,12 +27,25 @@ while(userInput != -1):
     # Loop for each round
     while(userInput != 0 or userInput != -1):
 
+        # Grab user input for this hand
         userInput = input("Enter value dealt : ")
         userInput = int(userInput)
 
+        # Reset values so they can be recounted
+        lowerCount = 0
+        higherCount = 0
+        equalCount = 0
+        totalCount = 0
+
+        # Check input and remove dealt card from the list
         if userInput in cardList:
             cardList.remove(userInput)
 
+        else:
+            print("Invalid Input\n\n\n\n")
+            break
+
+        # Track values for later calculations
         for card in cardList:
             totalCount += 1
 
@@ -45,8 +58,14 @@ while(userInput != -1):
             elif(card > userInput):
                 higherCount += 1
         
+        # Calculate final percentages
         percentLower = (lowerCount / totalCount) * 100
         percentHigher = (higherCount / totalCount) * 100
-        print("Lower%: " + str(round(percentLower, 0)) + "    " + "Higher%: " + str(round(percentHigher, 0)) + "\n")5
+        percentEqual = (equalCount / totalCount) * 100
+
+        # Print data for user to view
+        print("Lower%: " + str(round(percentLower, 0)) + "      " + "Higher%: " + str(round(percentHigher, 0))+ "       " + "Equal%: " + str(round(percentEqual, 0)) + "\n")
+        print("#Lower: " + str(lowerCount) + "      " + "#Higher: " + str(higherCount) + "      " + "#Equal: " + str(equalCount) + "\n")
+        print("Cards Left: " + str(totalCount) + "\n")
 
     
